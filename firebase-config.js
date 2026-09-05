85f73712bc8afb058be63b97c46c74ed6435109b
sveinboe-ai <svein@sbgroup.no>
2026-09-05 10:26:38 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDl83bd16lXl6VoxInSmBM6X4LRbrpo0mY",
+  authDomain: "jaerencognc.firebaseapp.com",
+  projectId: "jaerencognc",
+  storageBucket: "jaerencognc.firebasestorage.app",
+  messagingSenderId: "187404222586",
+  appId: "1:187404222586:web:2b2bcb9a61f6e328ab7bc3"
+};
